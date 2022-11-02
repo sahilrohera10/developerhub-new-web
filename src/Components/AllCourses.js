752f72a950c2572ff5f4290632764e1ae7d25c4a
YRH <yashhans479@gmail.com>
2022-11-02 16:28:21 +0530

    cards change

--- src/Components/AllCourses.js
+++ src/Components/AllCourses.js
@@ -4,40 +4,39 @@ import "./AllCourses.css";
 
 const CardArray = [
   {
-    img: "BlockchainImg1.jpeg",
-    text: "Blockchain technology courses for Multichain, Hyperledger, Ethereum, and Stellar, etc. amongst others.",
-    link: "/blockchain",
-  },
-  {
-    img: "AI_ML1.png",
-    text: "AI/ML course is an amalgamation of comprehensive and rigorous curriculum. It would cover key concepts and technologies of Artificial Intelligence and Machine Learning with higher utility value. ",
-    link: "/aiml",
-  },
-];
-const CardArray2 = [
-  {
-    img: "AR_VR1.png",
-    text: "AR/VR course is a great course for you if you want to take your imagination & creative skills to the next level. ",
-    link: "/ar-vr",
+    img: "MetaverseImg1.png",
+    title:"Metaverse",
+    text: "Metaverse development course that includes a 3D character developing avatars and for that you need to be skilled in Blender, Maya, Cinema 4D, etc. amongst others. ",
+    link: "/meta",
+    button:"Read More"
   },
   {
     img: "MetaverseImg1.png",
-    text: "Metaverse development course that includes a 3D character developing avatars and for that you need to be skilled in Blender, Maya, Cinema 4D, etc. amongst others. ",
+    title:"AI & ML",
+    text: "AI/ML course is an amalgamation of comprehensive and rigorous curriculum. It would cover key concepts and technologies of Artificial Intelligence and Machine Learning with higher utility value.",
     link: "/meta",
+    button:"Read More"
   },
-];
-
+  {
+    img: "MetaverseImg1.png",
+    title:"Blockchain",
+    text: "Blockchain technology courses for Multichain, Hyperledger, Ethereum, and Stellar, etc. amongst others. ",
+    link: "/meta",
+    button:"Read More"
+  },
+  {
+    img: "MetaverseImg1.png",
+    title:"AR & VR",
+    text: "AR/VR course is a great course for you if you want to take your imagination & creative skills to the next level. ",
+    link: "/meta",
+    button:"Read More"
+  },];
 export default function AllCourses() {
   return (
     <div className="AllCourses">
       <p className="animate-charcter">Explore Courses</p>
       <div className="Card_Div">
         {CardArray.map((d) => (
-          <Card data={d} />
-        ))}
-      </div>
-      <div className="Card_Div_2">
-        {CardArray2.map((d) => (
           <Card data={d} />
         ))}
       </div>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,21 +3,15 @@ import { Link } from "react-router-dom";
 import "./Card.css";
 export default function card(props) {
   return (
-    <div className="new-card">
-      <div className="front-new">
-        {/* <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-         */}
-        <img src={props.data.img} alt="" className="front_Img" />
-      </div>
-      <div className="back-new">
-        <div style={{ width: "280px", color: "black" }}>
-          <p>{props.data.text}</p>
-          {/* <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p> */}
-          <Link to={props.data.link}>
-            <button className="button-new">Read More</button>
-          </Link>
+        <div class="card" >
+          <div class="content">
+            <p class="title" style={{"fontSize":"30px"}}>{props.data.title}</p>
+            <p class="copy">{props.data.text}</p>
+            <Link to={props.data.link}>
+            <button class="btn">{props.data.button}</button>
+            </Link>
+          </div>
         </div>
-      </div>
-    </div>
+  
   );
 }
